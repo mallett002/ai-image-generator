61e4c4dbc39f5883942c7f07ccf491de1740b689
William Mallett <mallett002@gmail.com>
2024-07-13 16:14:16 -0500

    render index.html

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+import path from 'path';
 import express from 'express';
 import 'dotenv/config';
 
 import routes from './routes/openai-routes.js';
-
-// dotenv.config();
+import { fileURLToPath } from 'url';
 
 const port = process.env.PORT;
 const app = express();
 
+// Body parser
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+// Get current file path
+const currFilePath = fileURLToPath(import.meta.url);
+const dirName = path.dirname(currFilePath);
+
+// static folder
+app.use(express.static(path.join(dirName, 'public')));
 
 app.use('/openai', routes);
 
